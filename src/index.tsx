d03a9eb263dbeb7cb86f230e8188ed184b7bba1a
Morozov Anton <antonmorozoff64@gmail.com>
2022-06-26 20:38:36 +0200

    Scroll Restoration React Router Fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import "./assets/fonts/Mudkty-Wtvolkolak/mudkty-wtvolkolakgrotesque-light.ttf";
 import "./index.css";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
+import { ScrollToTop } from "./app/components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
