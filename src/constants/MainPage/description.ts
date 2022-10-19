8119c7b98e3f45f846b56cb90482fc0292a3e2e8
Morozov Anton <antonmorozoff64@gmail.com>
2022-10-19 19:51:49 +0200

    update: typos

--- src/constants/MainPage/description.ts
+++ src/constants/MainPage/description.ts
@@ -1,6 +1,6 @@
 export const firstPart = `It’s my portfolio! I am a designer from Ukraine. You can see my progress over the years of my practice in web design.`;
 
-export const secondPart = `I started my career as a graphic designer, graduated from the academy and after working in this field for a short time, I decided to move into the field of web design. Namely UX and UI. It became interesting for me to learn design from the side of bissnes, psychology and marketing.`;
+export const secondPart = `I started my career as a graphic designer, graduated from the academy and after working in this field for a short time, I decided to move into the field of web design. Namely UX and UI. It became interesting for me to learn design from the side of business, psychology and marketing.`;
 
 export const thirdPart = `I offer a solution to various business problems with the help of modern design and competent UX.`;
 
